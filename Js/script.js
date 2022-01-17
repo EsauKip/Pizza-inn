76dc7045dd7b832edeb40e5af4540c98682d5f61
EsauKip <esau.kiprono@student.moringaschool.com>
2022-01-17 05:05:08 +0300

    hide information,collect value of order

--- Js/script.js
+++ Js/script.js
@@ -17,4 +17,25 @@ $(document).ready(function() {
             // shows information to complete order
             $('.btn').click(function() {
                 $('.order').show();
+            }); // hides the information
+            $('#cancel').click(function() {
+                $("form")[0].reset();
+                $('.order').hide();
             });
+
+
+            $('form#order').last().submit(function(event) {
+
+                        event.preventDefault();
+
+                        // collects values of the order
+                        let size = parseInt($('input:radio[name=size]:checked').val());
+                        let crust = parseInt($('input:radio[name=crust]:checked').val());
+                        let topping = [];
+                        $.each($('input:checkbox[name=topping]:checked'), function() {
+                            topping.push(parseInt($(this).val()))
+                        });
+                        let totalToppings = 0;
+                        for (let i = 0; i < topping.length; i++) {
+                            totalToppings += topping[i];
+                        }
