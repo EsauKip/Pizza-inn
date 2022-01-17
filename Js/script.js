4fc5e87449bcdc57bcfa087d9d9998251b11545a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-01-17 05:06:30 +0300

    displace selected values in summary

--- Js/script.js
+++ Js/script.js
@@ -39,3 +39,25 @@ $(document).ready(function() {
                         for (let i = 0; i < topping.length; i++) {
                             totalToppings += topping[i];
                         }
+                        // display selected values in a summary
+                        let idSize = $('input:radio[name=size]:checked').attr('id');
+                        let idCrust = $('input:radio[name=crust]:checked').attr('id');
+                        let idToppings = [];
+                        $.each($('input:checkbox[name=topping]:checked'), function() {
+                            idToppings.push($(this).attr('id'));
+                        });
+
+                        let numberOfPizza = parseInt($('#orders-selected').val());
+
+                        $('.first-summary').show();
+                        $('.first-summary').append(
+                            "<tbody>" +
+                            "<tr class='table-dark'>" +
+                            "<td>" + "</td>" +
+                            "<td>" + idSize + "</td>" +
+                            "<td>" + idCrust + "</td>" +
+                            "<td>" + idToppings + "</td>" +
+                            "<td>" + numberOfPizza + "</td>" +
+                            "</tr>" +
+                            "</tbody>"
+                        );
